dbd4a2ccfedb3935cf021c362649c84d9fef58ef
mani2002 <manikantavinjamuri8522@gmail.com>
2024-05-02 13:08:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState(["task1", "task2"]);
+
+  const [input, setInput] = useState("");
+
+  function addTodo() {
+    if (input.trim().length === 0) {
+      alert("Input cannot be empty");
+    } else {
+      setTodo([...todo, input]);
+      setInput("");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="todo-div">
+        <h2>My Todos:</h2>
+        {todo.map((item, i) => (
+          <div className="flex">
+            <input type="checkbox" />
+            <p key={i}>{item}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex">
+        <input
+          type="text"
+          required
+          value={input}
+          placeholder="new task"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTodo}>Add todo</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
